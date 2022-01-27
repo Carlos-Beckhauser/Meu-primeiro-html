35ff20ec61241115d8c08f823ffe171a95a3af07
Carlos Beckhauser <cbeckhauser@gmail.com>
2022-01-27 20:30:06 -0300

    Adição de arquivo JavaScript

---
+++ app.js
@@ -0,0 +1,15 @@
+let resultado = 10 + 10;
+console.log ("Soma: ", resultado);
+
+resultado = 10 - 10;
+console.log ("Subtração: ", resultado);
+
+resultado = 10 * 10;
+console.log ("Multiplicação: ", resultado);
+
+resultado = 10 / 10;
+console.log ("Divisão: ", resultado) ;
+
+resultado = 10 % 3;
+console.log ("Resto: ", resultado);
+
