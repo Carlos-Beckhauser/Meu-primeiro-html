7bb9a2407039ede6d7f4633cb591435496cb9383
Carlos Beckhauser <cbeckhauser@gmail.com>
2022-02-18 15:08:51 -0300

    criado botao no JS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let resultado = 10 + 10;
+/* let resultado = 10 + 10;
 console.log ("Soma: ", resultado);
 
 resultado = 10 - 10;
@@ -11,5 +11,12 @@ resultado = 10 / 10;
 console.log ("Divisão: ", resultado) ;
 
 resultado = 10 % 3;
-console.log ("Resto: ", resultado);
+console.log ("Resto: ", resultado); */
 
+
+const buttonElement = document.getElementById('btn');
+
+
+buttonElement.addEventListener('click', function(event) {
+document.getElementById("nome").innerHTML = "Meu nome é Edson";
+})
