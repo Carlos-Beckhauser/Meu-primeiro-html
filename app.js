00926268ff7d71e8a83a9b4a40d9b7faf4bba6e9
Carlos Beckhauser <cbeckhauser@gmail.com>
2022-03-08 20:22:14 -0300

    deleted old button

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ console.log ("Divisão: ", resultado) ;
 resultado = 10 % 3;
 console.log ("Resto: ", resultado); */
 
-
-/* const buttonElement = document.getElementById('btn');
-
-
-buttonElement.addEventListener("click", function(event) {
-document.getElementById("nome").innerHTML = "Meu nome é Edson";
-}) */
-
-
 /* const professor = "Calinho Beckhauser";
 let idade = 27;
 const programador = false;
