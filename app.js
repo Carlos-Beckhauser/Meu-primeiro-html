65f734e40264955840233b78db2c9294a8a54ac1
Carlos Beckhauser <cbeckhauser@gmail.com>
2022-03-08 20:18:58 -0300

    Testing Button Functions

--- app.js
+++ app.js
@@ -17,18 +17,29 @@ console.log ("Resto: ", resultado); */
 /* const buttonElement = document.getElementById('btn');
 
 
-buttonElement.addEventListener('click', function(event) {
+buttonElement.addEventListener("click", function(event) {
 document.getElementById("nome").innerHTML = "Meu nome é Edson";
-})
- */
+}) */
 
-const professor = "Calinho Beckhauser";
-const idade = 27;
+
+/* const professor = "Calinho Beckhauser";
+let idade = 27;
 const programador = false;
 
 
-if (idade == 28) {
+if (idade === 28) {
     console.log ("A")
 } else {
     console.log ("B")
-}
+} */
+
+/* alert ("Warning !!!!");
+ */
+const p = document.getElementById("first");
+const btn = document.getElementById("botao1");
+/* p.innerHTML = "Editei essa linha com JS!!" */
+
+btn.addEventListener("click", function()  {
+    p.innerHTML = "Vá a Merda";
+});
+console.log(p);
